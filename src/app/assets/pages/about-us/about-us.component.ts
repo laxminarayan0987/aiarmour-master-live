e63dd3958d5c4dcc660390408613df8e102c7a7c
laxminarayan0987 <laxminarayandash20@gmail.com>
2024-12-26 13:52:13 +0530

    committed

--- src/app/assets/pages/about-us/about-us.component.ts
+++ src/app/assets/pages/about-us/about-us.component.ts
@@ -15,68 +15,48 @@ export class AboutUsComponent {
      heading : "Our Expertise",
       bodyList : [
         {
-          head: "Cybersecurity Solutions:  ",
+          head: "1. Cybersecurity Solutions:  ",
           body: "We offer advanced cybersecurity solutions to safeguard your organization's data, networks, and digital assets from evolving cyber threats. "
         },
         {
-          head: "AI & ML Research and Development:  ",
+          head: "2. AI & ML Research and Development:  ",
           body: "Our R&D team is at the forefront of innovation, advancing AI and Machine Learning technologies to address both business and educational challenges. "
         },
         {
-          head: "AI Implementation for Enhanced Productivity:  ",
+          head: "3. AI Implementation for Enhanced Productivity:  ",
           body: "We specialize in integrating AI into corporate environments to boost productivity. "
         },
         {
-          head: "Generative AI Integration:  ",
-          body: "ProtexAI helps businesses incorporate Generative AI into their workplaces, enabling creative solutions like content generation, chatbots, and automated insights. "
+          head: "4. Generative AI Integration:  ",
+          body: "CoatexAI helps businesses incorporate Generative AI into their workplaces, enabling creative solutions like content generation, chatbots, and automated insights. "
         },
         {
-          head: "Data Analytics & Insights:  ",
+          head: "5. Data Analytics & Insights:  ",
           body: "We harness the power of data analytics to help businesses make informed decisions."
         },
 	{
-          head: "Enterprise Application Development:  ",
+          head: "6. Enterprise Application Development:  ",
           body: "We build and modernize enterprise-grade applications that are secure, scalable, and tailored to meet the unique requirements of businesses. "
         },
 	{
-          head: "Secured AI Solutions:  ",
+          head: "7. Secured AI Solutions:  ",
           body: "We prioritize the responsible use of AI by ensuring that our solutions are secure, compliant, and privacy-focused."
         },
 	{
-          head: "AI-Enabled Threat Protection:  ",
+          head: "8. AI-Enabled Threat Protection:  ",
           body: "Our AI-enabled threat protection services leverage advanced AI models to proactively identify, analyze, and respond to potential threats."
         },
 	{
-          head: "NexGenAI Solutions:  ",
-          body: "At ProtexAI, we specialize in transforming the latest advancements in Generative AI and Deep Learning into real-world solutions, empowering a diverse range of industries, including Retail, Tourism, Publishing, Sports & Fitness, Customer Support, Education, and Job Training. "
+          head: "9. NexGenAI Solutions:  ",
+          body: "At CoatexAI, we specialize in transforming the latest advancements in Generative AI and Deep Learning into real-world solutions, empowering a diverse range of industries, including Retail, Tourism, Publishing, Sports & Fitness, Customer Support, Education, and Job Training. "
+        },
+        {
+          head: "10. Research Related to AI and Cyber Security:  ",
+          body: "We prioritize on the research and development related to cutting-edge technology along with cyber security solutions. "
         },
       ]
     },
-    {
-      heading : "About Our Co-working Place",
-      bodyList : [
-        {
-          head: "Startup India Recognition:  ",
-          body: "As a recognized startup by Startup India, we are committed to fostering innovation and entrepreneurship."
-        },
-        {
-          head: "ISO 9001:2015 Certification:  ",
-          body: "Our ISO certification reflects our dedication to maintaining the highest standards of quality in our processes and services."
-        },
-        {
-          head: "MSME Registration:  ",
-          body: "As a registered MSME, we adhere to ethical business practices and contribute to the economic development of the community."
-        },
-        {
-          head: "AICTE Internship Approval:  ",
-          body: "We are approved by AICTE to offer internships, ensuring we provide valuable learning experiences and contribute to skill development."
-        },
-        {
-          head: "Ministry of Corporate Affairs Incorporation:  ",
-          body: "Incorporated with the Ministry of Corporate Affairs, we operate with full compliance and transparency."
-        }
-      ]
-    }
+   
   ];
   public cardClass = "";
   constructor() {
--- src/app/assets/pages/training-internship/training-internship.component.ts
+++ src/app/assets/pages/training-internship/training-internship.component.ts
@@ -13,153 +13,60 @@ export class TrainingInternshipComponent {
       name : "AI/ML Research Intern",
       description : "Learn the Fundamentals and Grow Your Generative AI (GenAI) Skill Set.",
       icon: "assets/artificial-intelligence4.png",
-      badges: [
-        {
-          date: "02 Sep 24",
-          days: "Mon - Fri",
-          badgeType: "Regular",
-          type: "Online",
-          bookingStatus: "Closed",
-          timing: "01:30PM to 05:30PM"
-        },
-      ]
     },
     {
       name : "AI/ML Intern with Front-end Development",
       description : "Dive into the world of Python, versatile and powerful programming language.",
       icon: "assets/artificial-intelligence2.png",
-      badges: [
-        {
-          date: "02 Sep 24",
-          days: "Mon - Fri",
-          badgeType: "Regular",
-          type: "Online",
-          bookingStatus: "Closed",
-          timing: "01:30PM to 05:30PM"
-        },
-      ]
     },
     {
       name : "GenAI intern",
       description : "Dive into the world of Python, versatile and powerful programming language.",
       icon: "assets/artificial-intelligence3.png",
-      badges: [
-        {
-          date: "02 Sep 24",
-          days: "Mon - Fri",
-          badgeType: "Regular",
-          type: "Online",
-          bookingStatus: "Closed",
-          timing: "01:30PM to 05:30PM"
-        },
-      ]
     },
     {
       name : "Computer Vison Intern",
       description : "Dive into the world of Python, versatile and powerful programming language.",
       icon: "assets/ComputerVison.png",
-      badges: [
-        {
-          date: "02 Sep 24",
-          days: "Mon - Fri",
-          badgeType: "Regular",
-          type: "Online",
-          bookingStatus: "Closed",
-          timing: "01:30PM to 05:30PM"
-        },
-      ]
+   
     },
     {
       name : "Data Analyst",
       description : "Dive into the world of Python, versatile and powerful programming language.",
       icon: "assets/data-analyst.png",
-      badges: [
-        {
-          date: "02 Sep 24",
-          days: "Mon - Fri",
-          badgeType: "Regular",
-          type: "Online",
-          bookingStatus: "Closed",
-          timing: "01:30PM to 05:30PM"
-        },
-      ]
+     
     },
     {
       name : "Machine Learning Intern",
       description : "Dive into the world of Python, versatile and powerful programming language.",
       icon: "assets/artificial-intelligence.png",
-      badges: [
-        {
-          date: "02 Sep 24",
-          days: "Mon - Fri",
-          badgeType: "Regular",
-          type: "Online",
-          bookingStatus: "Closed",
-          timing: "01:30PM to 05:30PM"
-        },
-      ]
+    
     },
     {
       name : "CyberAI Intern",
       description : "Dive into the world of Python, versatile and powerful programming language.",
       icon: "assets/artificial-intelligence5.png",
-      badges: [
-        {
-          date: "02 Sep 24",
-          days: "Mon - Fri",
-          badgeType: "Regular",
-          type: "Online",
-          bookingStatus: "Closed",
-          timing: "01:30PM to 05:30PM"
-        },
-      ]
+     
     },
  {
       name : "VAPT Intern",
       description : "Dive into the world of Python, versatile and powerful programming language.",
       icon: "assets/testing.png",
-      badges: [
-        {
-          date: "02 Sep 24",
-          days: "Mon - Fri",
-          badgeType: "Regular",
-          type: "Online",
-          bookingStatus: "Closed",
-          timing: "01:30PM to 05:30PM"
-        },
-      ]
+    
     },
 
  {
       name : "Penetration Tester Intern",
       description : "Dive into the world of Python, versatile and powerful programming language.",
       icon: "assets/testing2.png",
-      badges: [
-        {
-          date: "02 Sep 24",
-          days: "Mon - Fri",
-          badgeType: "Regular",
-          type: "Online",
-          bookingStatus: "Closed",
-          timing: "01:30PM to 05:30PM"
-        },
-      ]
+    
     },
 
     {
       name : "Security Analyst Intern",
       description : "Become proficient in Java, a robust and widely-used programming language.",
       icon: "assets/security.png",
-      badges: [
-        {
-          date: "02 Sep 24",
-          days: "Mon - Fri",
-          badgeType: "Regular",
-          type: "Online",
-          bookingStatus: "Closed",
-          timing: "01:30PM to 05:30PM"
-        },
-      ]
+     
     }
   ];
   public cardClass = "";
