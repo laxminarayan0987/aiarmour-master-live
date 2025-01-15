2d9013fc34f0075b568e5284aa379dd9d4083580
unknown <sksoumitrakar@gmail.com>
2025-01-15 22:04:18 +0530

    homepage changes

--- src/app/assets/pages/home/home.component.ts
+++ src/app/assets/pages/home/home.component.ts
@@ -1,13 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { internship } from '../../../../assets/configurations/internships'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ RouterModule ],
+  imports: [ RouterModule, CommonModule ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  images: string[] = [
+    'assets/images/CS.jpg',
+    'assets/images/AI.jpg',
+    'assets/images/AI2.png'
+  ];
 
+  currentIndex: number = 0;
+  intervalId: any;
+  public internships = internship;
+
+  constructor() { }
+
+ngOnInit(): void {
+  this.startInterval();
+  console.log(this.internships)
+}
+startInterval() {
+  this.intervalId = setInterval(() => {
+    this.nextImage();
+  }, 4000);
+}
+nextImage(): void {
+  if (this.currentIndex < this.images.length - 1) {
+    this.currentIndex++;
+  } else {
+    this.currentIndex = 0;
+  }
+}
+
+prevImage(): void {
+  if (this.currentIndex > 0) {
+    this.currentIndex--;
+  } else {
+    this.currentIndex = this.images.length - 1;
+  }
+}
+  ngOnDestroy(): void {
+    // Clean up the interval when the component is destroyed to avoid memory leaks
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
--- src/app/assets/pages/training-internship/training-internship.component.ts
+++ src/app/assets/pages/training-internship/training-internship.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { internship } from '../../../../assets/configurations/internships'
+
 @Component({
   selector: 'app-training-internship',
   standalone: true,
@@ -8,67 +10,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './training-internship.component.css'
 })
 export class TrainingInternshipComponent {
-  public courceDetails = [
-    {
-      name : "AI/ML Research Intern",
-      description : "Learn the Fundamentals and Grow Your Generative AI (GenAI) Skill Set.",
-      icon: "assets/artificial-intelligence4.png",
-    },
-    {
-      name : "AI/ML Intern with Front-end Development",
-      description : "Dive into the world of Python, versatile and powerful programming language.",
-      icon: "assets/artificial-intelligence2.png",
-    },
-    {
-      name : "GenAI intern",
-      description : "Dive into the world of Python, versatile and powerful programming language.",
-      icon: "assets/artificial-intelligence3.png",
-    },
-    {
-      name : "Computer Vison Intern",
-      description : "Dive into the world of Python, versatile and powerful programming language.",
-      icon: "assets/ComputerVison.png",
-   
-    },
-    {
-      name : "Data Analyst",
-      description : "Dive into the world of Python, versatile and powerful programming language.",
-      icon: "assets/data-analyst.png",
-     
-    },
-    {
-      name : "Machine Learning Intern",
-      description : "Dive into the world of Python, versatile and powerful programming language.",
-      icon: "assets/artificial-intelligence.png",
-    
-    },
-    {
-      name : "CyberAI Intern",
-      description : "Dive into the world of Python, versatile and powerful programming language.",
-      icon: "assets/artificial-intelligence5.png",
-     
-    },
- {
-      name : "VAPT Intern",
-      description : "Dive into the world of Python, versatile and powerful programming language.",
-      icon: "assets/testing.png",
-    
-    },
-
- {
-      name : "Penetration Tester Intern",
-      description : "Dive into the world of Python, versatile and powerful programming language.",
-      icon: "assets/testing2.png",
-    
-    },
-
-    {
-      name : "Security Analyst Intern",
-      description : "Become proficient in Java, a robust and widely-used programming language.",
-      icon: "assets/security.png",
-     
-    }
-  ];
+  public courceDetails = internship;
   public cardClass = "";
   constructor() {
     console.log('Component initialized');
